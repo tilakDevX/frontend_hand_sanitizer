d1ccfbbeb8dfa56c17821254714c3d11798404aa
Developer Awdhesh <awdheshthinker@gmail.com>
2023-08-31 16:35:09 +0530

    some modification

---
+++ src/Components/Cart.jsx
@@ -0,0 +1,13 @@
+import { Heading } from '@chakra-ui/react';
+import React from 'react';
+
+function Cart(props) {
+    return (
+        <Heading>
+            this is cart component
+            
+        </Heading>
+    );
+}
+
+export default Cart;
